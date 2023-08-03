03ba75a0f1c815fbfcb998093a8a058a1ef7a301
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2023-08-03 01:51:15 -0400

    Finishing touches

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('open--menu')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const arrowLeft = document.querySelector('.arrow__left');
+    const arrowRight = document.querySelector('.arrow__right');
+    const charlotteReview = document.getElementById('charlotteReview');
+    const adamReview = document.getElementById('adamReview');
+  
+    arrowLeft.addEventListener('click', function() {
+      charlotteReview.style.display = 'block';
+      adamReview.style.display = 'none';
+    });
+  
+    arrowRight.addEventListener('click', function() {
+      charlotteReview.style.display = 'none';
+      adamReview.style.display = 'block';
+    });
+  });
